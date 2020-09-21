75cd7903763bea1108bb75a214937ee1f9283204
xinhui.zxh <xinhui.zxh@antfin.com>
2020-09-21 10:55:38 +0800

    Updates

---
+++ precache-manifest.6460e75e3d245bd4e7e003a82c98f764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47bff8041bfa826f5eb92891975195ef",
+    "url": "/react-virtual-list-demo/index.html"
+  },
+  {
+    "revision": "640bcbfd918cdff2df4e",
+    "url": "/react-virtual-list-demo/static/css/main.359d035f.chunk.css"
+  },
+  {
+    "revision": "5d97ee73f3cba0904215",
+    "url": "/react-virtual-list-demo/static/js/2.179c1bf0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-virtual-list-demo/static/js/2.179c1bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640bcbfd918cdff2df4e",
+    "url": "/react-virtual-list-demo/static/js/main.53300ef9.chunk.js"
+  },
+  {
+    "revision": "46dda0ae522954143240",
+    "url": "/react-virtual-list-demo/static/js/runtime-main.06641dba.js"
+  }
+]);

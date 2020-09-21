84f27f8fcd4619bf5b0246934c4758a3b9f9d3e6
xinhui.zxh <xinhui.zxh@antfin.com>
2020-09-21 10:48:37 +0800

    feat: build

---
+++ build/precache-manifest.4db136b5cb9ce1f8656db4fc27002242.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1941072c385b6ecc4dac67c428fcd1df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8befd63ce82c4b51c088",
+    "url": "/static/css/main.359d035f.chunk.css"
+  },
+  {
+    "revision": "45453dd6ad9e111059be",
+    "url": "/static/js/2.3b3d389b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b3d389b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8befd63ce82c4b51c088",
+    "url": "/static/js/main.6137e41b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
